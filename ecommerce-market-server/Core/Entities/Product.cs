56051d21a2f79ba7e0f4d79633942836d6514e3c
jpablopachar <jpablopachar1993@gmail.com>
2025-05-16 15:52:53 -0500

    feat(entity): agregar propiedades a la clase Product

--- ecommerce-market-server/Core/Entities/Product.cs
+++ ecommerce-market-server/Core/Entities/Product.cs
@@ -6,5 +6,10 @@
         public string? Description { get; set; }
         public int Stock { get; set; }
         public int BrandId { get; set; }
+        public Brand? Brand { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public decimal Price { get; set; }
+        public string? Image { get; set; }
     }
 }

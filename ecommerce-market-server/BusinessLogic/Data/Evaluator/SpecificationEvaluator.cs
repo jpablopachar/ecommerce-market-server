753a28cae17c9ee542e6ca158935ae536d27a5d5
jpablopachar <jpablopachar1993@gmail.com>
2025-05-17 10:56:33 -0500

    feat(evaluator): agregar clase SpecificationEvaluator para evaluación de especificaciones

---
+++ ecommerce-market-server/BusinessLogic/Data/Evaluator/SpecificationEvaluator.cs
@@ -0,0 +1,43 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Data.Evaluator
+{
+    /// <summary>
+    /// Evaluador de especificaciones para entidades que heredan de <see cref="BaseClass"/>.
+    /// </summary>
+    /// <typeparam name="T">Tipo de entidad que hereda de <see cref="BaseClass"/>.</typeparam>
+    /// <remarks>
+    /// Esta clase implementa <see cref="ISpecificationEvaluator{TEntity}"/> para definir la lógica de evaluación de especificaciones.
+    /// </remarks>
+    public class SpecificationEvaluator<T> where T : BaseClass
+    {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> spec)
+        {
+            if (spec.Criteria != null)
+            {
+                inputQuery = inputQuery.Where(spec.Criteria);
+            }
+
+            if (spec.OrderBy != null)
+            {
+                inputQuery = inputQuery.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                inputQuery = inputQuery.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                inputQuery = inputQuery.Skip(spec.Skip).Take(spec.Take);
+            }
+
+            inputQuery = spec.Includes.Aggregate(inputQuery, (current, include) => current.Include(include));
+
+            return inputQuery;
+        }
+    }
+}

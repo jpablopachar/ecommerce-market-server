0c26e50e0db245596d41e2e5bcb58c5fdab1b411
jpablopachar <jpablopachar1993@gmail.com>
2025-05-22 01:28:31 -0500

    feat(evaluator): agregar SecuritySpecificationEvaluator para filtrado y paginación

---
+++ ecommerce-market-server/BusinessLogic/Data/Evaluator/SecuritySpecificationEvaluator.cs
@@ -0,0 +1,50 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Data.Evaluator
+{
+    /// <summary>
+    /// Clase que evalúa una especificación y aplica sus criterios a una consulta de tipo <see cref="IQueryable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">El tipo de entidad que se está evaluando, que debe heredar de <see cref="BaseClass"/>.</typeparam>
+    /// <remarks>
+    /// Esta clase es responsable de aplicar los criterios de una especificación a una consulta de tipo <see cref="IQueryable{T}"/>.
+    /// </remarks>
+    public class SecuritySpecificationEvaluator<T> where T : BaseClass
+    {
+        /// <summary>
+        /// Aplica los criterios de filtrado, ordenación, paginación e inclusión de entidades relacionadas definidos en una especificación a una consulta de tipo <see cref="IQueryable{T}"/>.
+        /// </summary>
+        /// <param name="inputQuery">La consulta base sobre la que se aplicarán las condiciones de la especificación.</param>
+        /// <param name="spec">La especificación que contiene los criterios y configuraciones a aplicar sobre la consulta.</param>
+        /// <returns>Una consulta <see cref="IQueryable{T}"/> modificada según los criterios de la especificación proporcionada.</returns>
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> spec)
+        {
+
+            if (spec.Criteria != null)
+            {
+                inputQuery = inputQuery.Where(spec.Criteria);
+            }
+
+            if (spec.OrderBy != null)
+            {
+                inputQuery = inputQuery.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                inputQuery = inputQuery.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                inputQuery = inputQuery.Skip(spec.Skip).Take(spec.Take);
+            }
+
+            inputQuery = spec.Includes.Aggregate(inputQuery, (current, include) => current.Include(include));
+
+            return inputQuery;
+        }
+    }
+}
---
+++ ecommerce-market-server/BusinessLogic/Data/Evaluator/SpecificationEvaluator.cs
@@ -0,0 +1,52 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Data.Evaluator
+{
+    /// <summary>
+    /// Clase que evalúa una especificación y aplica sus criterios a una consulta de tipo <see cref="IQueryable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">El tipo de entidad que se está evaluando, que debe heredar de <see cref="BaseClass"/>.</typeparam>
+    /// <remarks>
+    /// Esta clase es responsable de aplicar los criterios de una especificación a una consulta de tipo <see cref="IQueryable{T}"/>.
+    /// </remarks>
+    public class SpecificationEvaluator<T> where T : BaseClass
+    {
+        /// <summary>
+        /// Aplica los criterios de una especificación a una consulta de tipo <see cref="IQueryable{T}"/>.
+        /// </summary>
+        /// <param name="inputQuery">La consulta de entrada sobre la que se aplicarán los filtros y modificaciones.</param>
+        /// <param name="spec">La especificación que define los criterios, ordenamientos, inclusiones y paginación.</param>
+        /// <returns>
+        /// Una nueva consulta <see cref="IQueryable{T}"/> que incorpora los filtros, ordenamientos, inclusiones y paginación definidos en la especificación.
+        /// </returns>
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> spec)
+        {
+
+            if (spec.Criteria != null)
+            {
+                inputQuery = inputQuery.Where(spec.Criteria);
+            }
+
+            if (spec.OrderBy != null)
+            {
+                inputQuery = inputQuery.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                inputQuery = inputQuery.OrderByDescending(spec.OrderByDescending);
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                inputQuery = inputQuery.Skip(spec.Skip).Take(spec.Take);
+            }
+
+            inputQuery = spec.Includes.Aggregate(inputQuery, (current, include) => current.Include(include));
+
+            return inputQuery;
+        }
+    }
+}

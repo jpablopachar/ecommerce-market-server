733aee6ac673e97d03e780bb9608f2a879e5f24a
jpablopachar <jpablopachar1993@gmail.com>
2025-05-22 19:17:21 -0500

    feat(api): agregar ErrorController para manejar errores de la API

---
+++ ecommerce-market-server/WebApi/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Errors;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        /// <summary>
+        /// Maneja los errores de la API y devuelve una respuesta JSON con el código de error.
+        /// </summary>
+        /// <param name="code">El código de error.</param>
+        /// <returns>Una respuesta JSON con el código de error.</returns>
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new CodeErrorResponse(code));
+        }
+    }
+}
